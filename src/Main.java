ff1b55386cc6882ebc2527f00dbfe04aa9fb501f
chicofullstack <chico_fullstack_2023@outlook.com>
2024-07-18 23:21:13 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,42 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devChico = new Dev();
+        devChico.setNome("Chico");
+        devChico.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Chico" + devChico.getConteudoInscritos());
+        devChico.progredir();
+        devChico.progredir();
+        devChico.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Chico" + devChico.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Chico" + devChico.getConteudosConcluidos());
+        System.out.println("XP:" + devChico.calcularTotalXp());
+
+
+        System.out.println("-------------");
+
+
+        Dev devEdna = new Dev();
+        devEdna.setNome("Edna");
+        devEdna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Edna" + devEdna.getConteudoInscritos());
+        devEdna.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Edna" + devEdna.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Edna" + devEdna.getConteudosConcluidos());
+        System.out.println("XP:" + devEdna.calcularTotalXp());
+
     }
 }
